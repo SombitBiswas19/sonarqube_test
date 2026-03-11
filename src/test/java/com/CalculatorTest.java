ca2c10cf2d03b9390c0cd4fb19f64b4882c6a4c3
SombitBiswas19 <sombis785@gmail.com>
2026-03-11 11:23:00 +0530

    sonar_second

--- src/test/java/com/CalculatorTest.java
+++ src/test/java/com/CalculatorTest.java
@@ -1,13 +1,14 @@
 package com;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
 
 public class CalculatorTest {
 
 	@Test
-	 void testadd() {
+	 public void testadd() {
 		Calculator calc=new Calculator();
 		int res=calc.add(2, 3);
 		assertEquals(5,res);
